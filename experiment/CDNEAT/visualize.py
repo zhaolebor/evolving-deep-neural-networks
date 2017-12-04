5c3b8a2d82100a2a872502623e173f23daa551ad
Zhuolun Zhao <zzhao1@swarthmore.edu>
2017-12-03 22:57:35 -0500

    cleanup

--- experiment/CDNEAT/visualize.py
+++ experiment/CDNEAT/visualize.py
@@ -10,6 +10,9 @@ import pickle
 import numpy as np
 import keras
 from keras.utils import plot_model
+from keras.layers import Conv2D, Input
+from keras.models import Model
+
 #from .config import Config
 
 def visualize_model(model_file):
@@ -26,11 +29,20 @@ def visualize_model(model_file):
 
     #visualize.draw_net(model, "_" + model_file)
     #model.fit(x_train_all, y_train_all, epochs=50)
-    #loss, fitness = model.evaluate(x_test, y_test)
-    #print("fitness", fitness)
     plot_model(model, to_file=model_file + '_model.png', show_shapes = True)
-    #plot_model(module_1, to_file='module_1.png', show_shapes = True)
-    #plot_model(module_2, to_file='module_2.png', show_shapes = True)
+
+def test_nonlinear():
+
+    # input tensor for a 3-channel 256x256 image
+    x = Input(shape=(256, 256, 3))
+    # 3x3 conv with 3 output channels (same as input channels)
+    y = Conv2D(3, (3, 3), padding='same')(x)
+    # this returns x + y.
+    z = keras.layers.add([x, y])
+
+    model = Model(inputs=x, outputs=z)
+    plot_model(model, to_file='nonlinear_model.png', show_shapes = True)
+
 
 
 
@@ -42,10 +54,15 @@ def visualize_module(chromo_file):
     chromo = pickle.load(fp)
     print(chromo)
     fp.close()
-    config.load('configCIFAR10')
-    inputs = keras.layers.Input(config.Config.input_nodes, name='input')
-    module = chromo.decode(inputs)
-    plot_model(module, to_file=chromo_file +'_module.png', show_shapes = True)
+    
+
+
+    #config.load('configCIFAR10')
+    #inputs = keras.layers.Input(config.Config.input_nodes, name='input')
+    #module = chromo.decode(inputs)
+    #plot_model(module, to_file=chromo_file +'_module.png', show_shapes = True)
+
+#------------------------------------------------------------------
     #visualize.draw_net(chromo, "_" + chromo_file)
     #brain = nn.create_ffphenotype(chromo)
     #fitness = eval_individual(brain, robot, sim, show_trail=True)
@@ -55,9 +72,11 @@ def visualize_module(chromo_file):
     #print("fitness", fitness)
 
 if __name__ == "__main__":
-    directory = "CIFARtest2/"
-    for i in range(11):
-        model_file = "CIFAR10_best_model_" + str(i)
-        visualize_model(directory + model_file)
-        module_file = "CIFAR10_m_best_chromo_" + str(i)
-        visualize_module(directory + module_file)
+    #directory = "CIFARtest2/"
+    # for i in range(11):
+    #     model_file = "CIFAR10_best_model_" + str(i)
+    #     visualize_model(directory + model_file)
+    #     module_file = "CIFAR10_m_best_chromo_" + str(i)
+    #     visualize_module(directory + module_file)
+
+    test_nonlinear()
