0fec881c18414706a3c4381b50935d2fcf034f50
Zhuolun Zhao <zzhao1@swarthmore.edu>
2017-12-06 14:46:46 -0500

    training data equal distribution

--- experiment/CDNEAT/demo-cifar10.py
+++ experiment/CDNEAT/demo-cifar10.py
@@ -7,7 +7,7 @@
 # 
 # The following notebook demonstrates using CoDeepNEAT to solve CIFAR-10
 
-# In[1]:
+# In[11]:
 
 
 from keras.datasets import cifar10
@@ -24,7 +24,7 @@ import keras
 # 
 # Conveniently, it's also built into Keras, which our CoDeepNEAT imiplementation is built off of.
 
-# In[2]:
+# In[12]:
 
 
 (x_train_all, y_train_all), (x_test, y_test) = cifar10.load_data()
@@ -33,17 +33,39 @@ y_test = y_test[:,0]
 
 x_train_all = np.reshape(x_train_all, (x_train_all.shape[0], 32, 32, 3)).astype('float32') / 255
 x_test = np.reshape(x_test, (x_test.shape[0], 32, 32, 3)).astype('float32') / 255
-y_train_all = keras.utils.np_utils.to_categorical(y_train_all)
+y_train_all_one_hot = keras.utils.np_utils.to_categorical(y_train_all)
 y_test = keras.utils.np_utils.to_categorical(y_test)
+
+num_categories = 10
+category_count = np.zeros(num_categories)
+
+num_training = 42500
+
+x_train = []
+y_train = []
+x_val = []
+y_val = []
+
 
 index = np.array(range(len(x_train_all)))
 np.random.shuffle(index)
-index_train = index[:42500]
-index_val = index[42500:]
-x_train = x_train_all[index_train]
-y_train = y_train_all[index_train]
-x_val = x_train_all[index_val]
-y_val = y_train_all[index_val]
+x_train_all = x_train_all[index]
+y_train_all = y_train_all[index]
+y_train_all_one_hot = y_train_all_one_hot[index]
+for i in range(len(index)):
+    if category_count[y_train_all[i]] < num_training/num_categories:
+        x_train.append(x_train_all[i])
+        y_train.append(y_train_all_one_hot[i])
+        category_count[y_train_all[i]] += 1
+    else:
+        x_val.append(x_train_all[i])
+        y_val.append(y_train_all_one_hot[i])
+print(category_count)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_val = np.array(x_val)
+y_val = np.array(y_val)
 
 data = [x_train, y_train, x_val, y_val, x_test, y_test]
 print("data shapes")
@@ -61,13 +83,17 @@ print("  y test:", y_test.shape)
 # 
 # Many of the options and inputs are still handled through the config file. The config file has been shortened considerably as many parameters have been eliminated, although many parameters have also been introduced which could be added.
 
-# In[7]:
+# In[3]:
+
+
+get_ipython().run_cell_magic('file', 'configCIFAR10', '#--- parameters for the robot experiment ---#\n[phenotype]\ninput_nodes         = 32,32,3\noutput_nodes        = 10\nconv                = True\nLSTM                = False\n\n[genetic]\nmax_fitness_threshold = 1\n\n# Human reasoning\npop_size              = 10\nprob_addconv          = 0.9\nprob_addLSTM          = 0.0\nprob_addlayer         = 0.1\nprob_mutatelayer      = 0.3\nprob_addmodule        = 0.05\nprob_switchmodule     = 0.1\nelitism               = 1\n\n[genotype compatibility]\ncompatibility_threshold = 3.0\ncompatibility_change    = 0.0\nexcess_coefficient      = 5.0\ndisjoint_coefficient    = 3.0\nconnection_coefficient  = 0.4\nsize_coefficient        = 0.8\n\n[species]\nspecies_size        = 10\nsurvival_threshold  = 0.2\nold_threshold       = 30\nyouth_threshold     = 10\nold_penalty         = 0.2\nyouth_boost         = 1.2\nmax_stagnation      = 15')
+
 
 # ### Fitness
 # 
 # For this demonstration we'll be using supervised learning to train the networks produced by CoDeepNEAT on CIFAR-10 and will use their accuracy after 5 epochs as our fitness. CIFAR-10, like MNIST, is a 10 category classification problem.
 
-# In[8]:
+# In[13]:
 
 
 def fitness(network, data):
@@ -80,7 +106,7 @@ def fitness(network, data):
 # 
 # Evolution with CoDeepNEAT is slightly different than evolution with NEAT. The main difference is coevolution, where we have two separate populations with a hierarchical relationship evolving together.
 
-# In[9]:
+# In[5]:
 
 
 def evolve(n, debugging=False):
@@ -106,26 +132,8 @@ def evolve(n, debugging=False):
     #visualize.plot_species(blueprint_pop.species_log, name="NMISTbp_")
 
 
-# In[10]:
+# In[6]:
 
 
 evolve(25, True)
-
-
-# In[ ]:
-
-
-def eval_best(model_file):
-    config.load('configCIFAR10')
-    model = keras.models.load_model(model_file)
-    visualize.draw_net(model, "_" + model_file)    
-    model.fit(x_train_all, y_train_all, epochs=50)
-    loss, fitness = model.evaluate(x_test, y_test)
-    print("fitness", fitness)
-
-
-# In[ ]:
-
-
-# eval_best("CIFAR10_best_model_0")
 
