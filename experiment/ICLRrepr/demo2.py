bfa8003dd0aebe2e4a7eaf423a5c0bdd204c4ffc
Zhuolun Zhao <zzhao1@swarthmore.edu>
2017-12-15 03:22:44 -0500

    iclr

--- experiment/ICLRrepr/demo2.py
+++ experiment/ICLRrepr/demo2.py
@@ -4,7 +4,7 @@
 # # ICLR18repr demo
 # ## CS081 project checkpoint demo
 # ### Harsha Uppli, Alan Zhao, Gabriel Meyer-Lee
-# 
+#
 # The following notebook demonstrates using Hierarchichal Architecture search on MNIST
 
 # In[1]:
@@ -17,6 +17,7 @@ import pickle
 import numpy as np
 import keras
 import h5py
+from keras.preprocessing.image import ImageDataGenerator
 
 
 
@@ -51,23 +52,97 @@ def assemble_small(architecture, inputdim):
     l5 = keras.layers.GlobalAveragePooling2D()(l4)
     outputs = keras.layers.Dense(10, activation='softmax')(l5)
     model = keras.models.Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    sgd = keras.optimizers.SGD(lr=0.01, momentum=0.9, decay=0.0003, nesterov=False)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+def assemble_large(architecture, inputdim):
+    inputs = keras.layers.Input(shape=inputdim)
+    l1 = keras.layers.Conv2D(64, kernel_size=3, activation='relu')(inputs)
+    c1 = architecture.assemble(l1, 64)
+    l2 = keras.layers.SeparableConv2D(64, kernel_size=3, strides=1, activation='relu')(c1)
+    c2 = architecture.assemble(l2, 64)
+    l3 = keras.layers.SeparableConv2D(128, kernel_size=3, strides=2, activation='relu')(c2)
+    c3 = architecture.assemble(l3, 128)
+    l4 = keras.layers.SeparableConv2D(128, kernel_size=3, strides=1, activation='relu')(c3)
+    c4 = architecture.assemble(l4, 128)
+    l5 = keras.layers.SeparableConv2D(256, kernel_size=3, strides=2, activation='relu')(c2)
+    c5 = architecture.assemble(l5, 256)
+    l6 = keras.layers.SeparableConv2D(256, kernel_size=3, strides=1, activation='relu')(c2)
+    c6 = architecture.assemble(l6, 256)
+    l7 = keras.layers.SeparableConv2D(256, kernel_size=3, strides=1, activation='relu')(c2)
+    l8 = keras.layers.GlobalAveragePooling2D()(l7)
+    outputs = keras.layers.Dense(10, activation='softmax')(l8)
+    model = keras.models.Model(inputs=inputs, outputs=outputs)
+    sgd = keras.optimizers.SGD(lr=0.01, momentum=0.9, decay=0.0003, nesterov=False)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 
 # ### Fitness
-# 
+#
 # For this demonstration we'll be using supervised learning to train the networks produced by the architecture on NMIST and then use their accuracy after 10 epochs as their fitness.
 
 # In[4]:
 
 
 def fitness(architecture, data):
-    network = assemble_small(architecture, data[0].shape[1:])
-    network.fit(data[0], data[1],  epochs=5)
-    loss, acc = network.evaluate(data[2], data[3])
+    crop_size = 24
+    batch_size = 256
+    num_epoch = 5
+    eval_network = assemble_small(architecture, data[0].shape[1:])
+    train_network = assemble_small(architecture.copy(), (crop_size, crop_size, data[0].shape[-1]))
+
+
+    datagen = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+            width_shift_range=0,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0,  # randomly shift images vertically (fraction of total height)
+            horizontal_flip=True,  # randomly flip images
+            vertical_flip=False)  # randomly flip images
+
+    #network.fit_generator(datagen.flow(data[0], data[1], batch_size=batch_size),
+    #                    steps_per_epoch=data[0].shape[0] // batch_size,
+    #                    validation_data=(data[2], data[3]),
+    #                    epochs=num_epoch, verbose=1, max_queue_size=100)
+    for e in range(num_epoch):
+        print('Epoch:', e)
+        batches = 0
+        for x_batch, y_batch in datagen.flow(data[0], data[1], batch_size=batch_size):
+            new_x_batch = []
+            for x in x_batch:
+                cropped_x = random_crop(x, crop_size)
+                new_x_batch.append(cropped_x)
+            new_x_batch = np.array(new_x_batch)
+            train_network.fit(new_x_batch, y_batch, batch_size=batch_size, verbose=0)
+            batches += 1
+            if batches >= len(data[0]) / batch_size:
+            # we need to break the loop by hand because
+            # the generator loops indefinitely
+                break
+
+    weights = train_network.get_weights()
+    eval_network.set_weights(weights)
+
+    loss, acc = eval_network.evaluate(data[2], data[3])
     architecture.fitness = acc
-    return acc
+    return acc, eval_network.count_params()
+
+
+def random_crop(image, crop_size):
+    height, width = image.shape[0:2]
+    dx = crop_size
+    dy = crop_size
+    if width < dx or height < dy:
+        return None
+    x = np.random.randint(0, width - dx + 1)
+    y = np.random.randint(0, height - dy + 1)
+    return image[y:(y+dy), x:(x+dx),:]
 
 
 # ### Random Search
@@ -81,24 +156,31 @@ def random_mutate(Q, M, steps, data, record=None):
         print('\n---------------Mutating step: ' + str(i) +'---------------\n')
         if len(Q) > 0:
             indiv = Q.pop()
-            indiv.mutate()
-            acc = fitness(indiv, data)
+            acc, num_params = fitness(indiv, data)
             if record is not None:
-                record.write(str(i)+' Fitness: '+str(acc))
+                f = open(record,'a')
+                f.write(str(i)+' Fitness: '+str(acc)+' Params: '+str(num_params)+'\n')
+                f.close()
             M.append(indiv)
         else:
             indiv = random.choice(M).copy()
             indiv.mutate()
-            acc = fitness(indiv, data)
+            acc, num_params = fitness(indiv, data)
             if record is not None:
-                record.write(str(i)+' Fitness: '+str(acc))
+                f = open(record,'a')
+                f.write(str(i)+' Fitness: '+str(acc)+' Params: '+str(num_params)+'\n')
+                f.close()
             M.append(indiv)
 
-
+def initial_mutate(pop):
+    large_number = 50
+    for indiv in pop:
+        for i in range(large_number):
+            indiv.mutate()
 
 
 # ### Evolution
-# 
+#
 # Evolution with CoDeepNEAT is slightly different than evolution with NEAT. The main difference is coevolution, where we have two separate populations with a hierarchical relationship evolving together.
 
 # In[10]:
@@ -112,91 +194,101 @@ def tournament_select(pop):
     contestants.reverse()
     return contestants[0].copy()
 
-def evolve(n, data, record=None):
-    Q = create_flat_population(10, 8)
+def evolve(Q, n, data, record=None):
     init_pop = len(Q)
     M = []
     random_mutate(Q,M, init_pop, data, record)
-    for step in range(n-init_pop):
+    print(len(M))
+    M.sort()
+    M.reverse()
+    best = M[0]
+    file = open('best_random_arch','wb')
+    pickle.dump(best, file)
+    file.close()
+    for step in range(init_pop, n):
         indiv = tournament_select(M)
         indiv.mutate()
-        acc = fitness(indiv, data)
+        acc, num_params = fitness(indiv, data)
+        if acc > best.fitness:
+            best = indiv
+            file = open('best_evol_arch','wb')
+            pickle.dump(best, file)
+            file.close()
         if record is not None:
-                record.write(str(step+init_pop)+' Fitness: '+str(acc))
+                f = open(record,'a')
+                f.write(str(step)+' Fitness: '+str(acc)+' Params: '+str(num_params)+'\n')
+                f.close()
         M.append(indiv)
 
-
-
-
-# In[7]:
-
-
-def eval_best(model_file):
-    model = keras.models.load_model(model_file)
-    visualize.draw_net(model, "_" + model_file)    
-    model.fit(data[0], data[1], epochs=5)
-    loss, fitness = model.evaluate(data[4], data[5])
-    print("fitness", fitness)
-
-
-# In[8]:
 
 
 #eval_best("MNIST_best_model_0")
 
 # ### Sample problem: MNIST data set
-# 
+#
 # The MNIST data set of handwritten gray scale images of digits 0-9 is a classic computer vision data set and therefore makes for good testing. Conveniently, it's also built into Keras, which our CoDeepNEAT imiplementation is built off of.
 
 def main():
-  # In[2]: load data
-  (x_train_all, y_train_all), (x_test, y_test) = keras.datasets.cifar10.load_data()
-  y_train_all = y_train_all[:,0]
-  y_test = y_test[:,0]
+    # In[2]: load data
+    (x_train_all, y_train_all), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    y_train_all = y_train_all[:,0]
+    y_test = y_test[:,0]
 
-  x_train_all = np.reshape(x_train_all, (x_train_all.shape[0], 32, 32, 3)).astype('float32') / 255
-  x_test = np.reshape(x_test, (x_test.shape[0], 32, 32, 3)).astype('float32') / 255
-  y_train_all = keras.utils.np_utils.to_categorical(y_train_all)
-  y_test = keras.utils.np_utils.to_categorical(y_test)
+    x_train_all = np.reshape(x_train_all, (x_train_all.shape[0], 32, 32, 3)).astype('float32') / 255
+    x_test = np.reshape(x_test, (x_test.shape[0], 32, 32, 3)).astype('float32') / 255
+    y_train_all_one_hot = keras.utils.np_utils.to_categorical(y_train_all)
+    y_test = keras.utils.np_utils.to_categorical(y_test)
 
-  index = np.array(range(len(x_train_all)))
-  np.random.shuffle(index)
-  index_train = index[:42500]
-  index_val = index[42500:]
-  x_train = x_train_all[index_train]
-  y_train = y_train_all[index_train]
-  x_val = x_train_all[index_val]
-  y_val = y_train_all[index_val]
+    num_categories = 10
+    category_count = np.zeros(num_categories)
 
-  data = [x_train, y_train, x_val, y_val, x_test, y_test]
-  print("data shapes")
-  print("  x train:", x_train.shape)
-  print("  y train:", y_train.shape)
+    num_training = 40000
 
-  print("  x val:", x_val.shape)
-  print("  y val:", y_val.shape)
+    x_train = []
+    y_train = []
+    x_val = []
+    y_val = []
 
-  print("  x test:", x_test.shape)
-  print("  y test:", y_test.shape)
 
-  # In[7]: random search over flat population
-  record = open('fitnesses.txt','w')
-  Q = create_hier_population(10, [4, 5], [6, 1], 3)
-  M = []
-  random_mutate(Q, M, 100, data, record)
-  #evolve(20, data)
-  M.sort()
-  M.reverse()
-  top = 1
-  record.close()
-  for indiv in M[:5]:
-    filename = 'model_top_'+str(top)
-    indiv.save(filename)
-    top += 1
+    index = np.array(range(len(x_train_all)))
+    np.random.shuffle(index)
+    x_train_all = x_train_all[index]
+    y_train_all = y_train_all[index]
+    y_train_all_one_hot = y_train_all_one_hot[index]
+    for i in range(len(index)):
+        if category_count[y_train_all[i]] < num_training/num_categories:
+            x_train.append(x_train_all[i])
+            y_train.append(y_train_all_one_hot[i])
+            category_count[y_train_all[i]] += 1
+        else:
+            x_val.append(x_train_all[i])
+            y_val.append(y_train_all_one_hot[i])
+    print(category_count)
 
-  # In[6]: evolve over flat pop
-  #evolve(5, data, False)
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    x_val = np.array(x_val)
+    y_val = np.array(y_val)
+
+    data = [x_train, y_train, x_val, y_val, x_test, y_test]
+    print("data shapes")
+    print("  x train:", x_train.shape)
+    print("  y train:", y_train.shape)
+
+    print("  x val:", x_val.shape)
+    print("  y val:", y_val.shape)
+
+    print("  x test:", x_test.shape)
+    print("  y test:", y_test.shape)
+
+    # In[7]: random search over flat population
+    record = 'data.txt'
+    Q = create_hier_population(50, [4, 5], [6, 1], 3)
+    initial_mutate(Q)
+    evolve(Q, 200, data, record)
+
+    # In[6]: evolve over flat pop
+    #evolve(5, data, False)
 
 if __name__ == "__main__":
   main()
-
--- experiment/ICLRrepr/hierarchy.py
+++ experiment/ICLRrepr/hierarchy.py
@@ -8,13 +8,11 @@ import random
 import numpy as np
 
 class Motif(object):
-    def __init__(self, num_nodes, prev_motifs=[], level=2):
+    def __init__(self, num_nodes, prev_motifs=[], level=2, G=None):
         if level < 0:
             raise ValueError('Invalid level value')
         self.__size = num_nodes
         self.__level = level
-        # initialize node data as nothing
-        self.__x = []
         # if lowest non primitive level operations are primitive
         if level == 2:
             self.__o = [ None,'no_op', 'ident','1x1','3x3_depth','3x3_sep', 'max_pool', 'avg_pool']
@@ -24,14 +22,18 @@ class Motif(object):
             self.__o.extend(prev_motifs)
         # initialize adjacency matrix to encode linear chain of identity connections
         ones = np.ones((num_nodes, num_nodes), dtype=int)
-        self.__G = np.tril(ones, -1)
-        for j in range(1,num_nodes):
-            self.__G.itemset((j,j-1), 2)
+        if G is not None:
+            self.__G = G
+        else:
+            self.__G = np.tril(ones, -1)
+            for j in range(1,num_nodes):
+                self.__G.itemset((j,j-1), 2)
+
     level = property(lambda self: self.__level)
 
     def decode(self, inputs, c):
-        self.__x = []
-        self.__x.append(inputs)
+        x_tens = []
+        x_tens.append(inputs)
         if self.__level > 2:
             for i in range(1, self.__size):
                 new_x = []
@@ -42,16 +44,17 @@ class Motif(object):
                     elif op == 'no_op':
                         continue
                     elif op == 'ident':
-                        new_x.append(self.__x[j])
+                        new_x.append(x_tens[j])
                     else:
-                        new_x.append(op.decode(self.__x[j], c))
+                        new_x.append(op.decode(x_tens[j], c))
                 if len(new_x) == 0:
                     self.__G.itemset((i,i-1), 2)
-                    self.__x.append(self.__x[i-1])
+                    x_tens.append(x_tens[i-1])
                 elif len(new_x) == 1:
-                    self.__x.append(new_x[0])
+                    x_tens.append(new_x[0])
                 else:
-                    self.__x.append(keras.layers.Concatenate()(new_x))
+                    x2 = keras.layers.Concatenate()(new_x)
+                    x_tens.append(keras.layers.Conv2D(c, 1, padding='same', activation='relu')(x2))
         elif self.__level == 2:
             for i in range(1, self.__size):
                 new_x = []
@@ -60,36 +63,39 @@ class Motif(object):
                     if op is None or op == 'no_op':
                         continue
                     elif op == 'ident':
-                        new_x.append(self.__x[j])
+                        new_x.append(x_tens[j])
                     elif op == '1x1':
-                        x_temp = keras.layers.Conv2D(c, 1, padding='same', activation='relu')(self.__x[j])
+                        x_temp = keras.layers.Conv2D(c, 1, padding='same', activation='relu')(x_tens[j])
                         new_x.append(keras.layers.BatchNormalization()(x_temp))
                     elif op == '3x3_depth':
-                        x_temp = keras.layers.Conv2D(c, 3, padding='same', activation='relu')(self.__x[j])
+                        x_temp = keras.layers.Conv2D(c, 3, padding='same', activation='relu')(x_tens[j])
                         new_x.append(keras.layers.BatchNormalization()(x_temp))
                     elif op == '3x3_sep':
-                        x_temp = keras.layers.SeparableConv2D(c, 3, padding='same', activation='relu')(self.__x[j])
+                        x_temp = keras.layers.SeparableConv2D(c, 3, padding='same', activation='relu')(x_tens[j])
                         new_x.append(keras.layers.BatchNormalization()(x_temp))
                     elif op == 'max_pool':
-                        new_x.append(keras.layers.MaxPooling2D(3, strides=1, padding='same')(self.__x[j]))
+                        new_x.append(keras.layers.MaxPooling2D(3, strides=1, padding='same')(x_tens[j]))
                     elif op == 'avg_pool':
-                        new_x.append(keras.layers.AveragePooling2D(3, strides=1, padding='same')(self.__x[j]))
+                        new_x.append(keras.layers.AveragePooling2D(3, strides=1, padding='same')(x_tens[j]))
                     else:
                         raise ValueError('Invalid level 1 motif: '+op)
                 if len(new_x) == 0:
                     self.__G.itemset((i,i-1), 2)
-                    self.__x.append(self.__x[i-1])
+                    x_tens.append(x_tens[i-1])
                 elif len(new_x) == 1:
-                    self.__x.append(new_x[0])
+                    x_tens.append(new_x[0])
                 else:
-                    self.__x.append(keras.layers.Concatenate()(new_x))
-        assert len(self.__x) == self.__size, 'Size Error: ' + str(len(self.__x)) + ' ' + str(self.__size)
-        return self.__x[-1]
+                    x_tens.append(keras.layers.Concatenate()(new_x))
+        assert len(x_tens) == self.__size, 'Size Error: ' + str(len(x_tens)) + ' ' + str(self.__size)
+        return x_tens[-1]
 
     def mutate(self):
         i = random.randrange(1, self.__size)
         j = random.randrange(i)
         self.__G.itemset((i,j), random.randrange(1,len(self.__o)))
+
+    def copy(self):
+        return Motif(self.__size, self.__o[3:], self.__level, self.__G)
 
 class Architecture(object):
     def __init__(self, num_nodes):
@@ -142,7 +148,6 @@ class HierArch(Architecture):
             for i in range(0, num_levels-1):
                 self.__m.insert(i,[])
                 for j in range(num_motifs[i]):
-                    print(num_nodes[i])
                     if i == 0:
                         self.__m[i].append(Motif(num_nodes[i]))
                     else:
@@ -159,6 +164,7 @@ class HierArch(Architecture):
         return output
 
     def copy(self):
-        return HierArch(self._num_nodes, self.height, self._num_motifs, motifs=self.__m)
-
-
+        new_m = []
+        for motif in self.__m:
+            new_m.append(motif.copy())
+        return HierArch(self._num_nodes, self.height, self._num_motifs, motifs=new_m)
